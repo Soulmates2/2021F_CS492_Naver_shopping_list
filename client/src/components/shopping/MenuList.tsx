69c788711dac81ead25230530af79ceefdd472bc
윤하나 <hana000510@naver.com>
2021-11-14 02:03:39 +0900

    productList

--- client/src/components/shopping/MenuList.tsx
+++ client/src/components/shopping/MenuList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
 import { getChannelMenu } from '../../lib/api/shopping';
 import Menu from './Menu';
 interface MenuListProps {
@@ -9,12 +9,12 @@ const MenuList = (props: MenuListProps) => {
   const [MenuList, setMenuList] = useState([]);
   //API와 연동하여 해당 채널의 모든 메뉴들을 menuList에 세팅합니다.
   //메뉴는 몇가지 정보가 더 있어서 수정예정입니다.
-  useEffect(() => {
-    (async () => {
-      const { data } = await getChannelMenu(props.channelID);
-      setMenuList(data.menus);
-    })();
-  }, [props]);
+
+  useMemo(async () => {
+    const { data } = await getChannelMenu(props.channelID);
+    setMenuList(data.menus);
+  }, [props.channelID]);
+
   //메뉴리스트들로 메뉴 컴포넌트의 리스트를 만듭니다.
   return (
     <div className="menuList">
--- client/src/components/shopping/Product.tsx
+++ client/src/components/shopping/Product.tsx
@@ -5,21 +5,38 @@ import './ProductList.css';
 
 
 //product info의 내용과 타입들입니다.
-//이 내용들로 info.???를 사용해 상품카드를 만들면 됩니다.
+//이 내용들로 상품카드를 만들면 됩니다.
+interface ProductInfoProps {
+  channel: object;
+  menus: Array<string>;
+  modDate: Date;
+  name: string;
+  productImages: Array<imageInfo>;
+  salePrice: number;
+  soldout: boolean;
+  _id: string;
+  view: viewDibsType;
+  dibs: viewDibsType;
+}
 
-const Product = (props: any) => {
+interface imageInfo {
+  imageType: string;
+  order: number;
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface viewDibsType {
+  total: number;
+}
+
+interface info {
+  info: ProductInfoProps;
+}
+
+const Product = (props: info) => {
   const { info } = props;
-  // import img from info.productImages[0].url;
-  // class ImageComponent extends Component {
-  //   render() {
-  //     return (
-  //         <div>
-  //             <img src={info.productImages[0].url} alt="display image" />
-  //         </div>
-  //     );
-  //   }
-  // }
-
   const viewUpdate = () => {
     console.log(props);
     viewPatchProduct(info._id);
@@ -32,13 +49,14 @@ const Product = (props: any) => {
       <div className="product" onClick={viewUpdate}>
         <div className="item_card">
           <div className="pic">
+            {/* 현재 이미지가 불러와지지 않는데 이유를 잘 모르겠습니다. */}
             <img src={info.productImages[0].url} className="img" alt="no image found" />
             {/* <img src={require(info.productImages[0].url)} className="img" alt="no image found" /> */}
           </div>
           <p className="i_price">{cn1}원</p>
           <p className="i_name">{info.name}</p>
-          <span className="i_view">Views: {info.view['total']}  | </span>
-          <span className="i_like">Likes: 0</span>
+          <span className="i_view">Views: {info.view.total}  | </span>
+          <span className="i_like">Likes: {info.dibs.total}</span>
           <button className="button_like" aria-current="false">
             <span>찜하기</span>
           </button>
