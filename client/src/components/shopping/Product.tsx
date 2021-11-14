5f5ea46822f35c4dfb43f78ed7fbfe8bdbab25b8
윤하나 <hana000510@naver.com>
2021-11-14 18:14:49 +0900

    productcard with no image

--- client/src/components/shopping/Product.tsx
+++ client/src/components/shopping/Product.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { viewPatchProduct } from '../../lib/api/shopping';
+import './ProductList.css';
+
 
 //product info의 내용과 타입들입니다.
 //이 내용들로 상품카드를 만들면 됩니다.
@@ -36,21 +38,31 @@ interface info {
 const Product = (props: info) => {
   const { info } = props;
   const viewUpdate = () => {
+    console.log(props);
     viewPatchProduct(info._id);
   };
+  const n1 = info.salePrice;
+  const cn1 = n1.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+
   return (
-    <div className="product" onClick={viewUpdate}>
-      <Link to={{ pathname: `/products/${info._id}`, state: info }}>
-        {/* 현재 이미지가 불러와지지 않는데 이유를 잘 모르겠습니다. */}
-        {/* <img src={info.productImages[0].url} alt="productimage" /> */}
-        {info.productImages[0].url}
-        <br />
-        {info.name}
-        <br />
-        조회수:{info.view.total}
-        <br />찜 : {info.dibs.total}
-      </Link>
-    </div>
+    <Link to={{ pathname: `/products/${info._id}`, state: info }}>
+      <div className="product" onClick={viewUpdate}>
+        <div className="item_card">
+          <div className="pic">
+            {/* 현재 이미지가 불러와지지 않는데 이유를 잘 모르겠습니다. */}
+            <img src={info.productImages[0].url} className="img" alt="no image found" />
+            {/* <img src={require(info.productImages[0].url)} className="img" alt="no image found" /> */}
+          </div>
+          <p className="i_price">{cn1}원</p>
+          <p className="i_name">{info.name}</p>
+          <span className="i_view">Views: {info.view.total}  | </span>
+          <span className="i_like">Likes: {info.dibs.total}</span>
+          <button className="button_like" aria-current="false">
+            <span>찜하기</span>
+          </button>
+        </div>
+      </div>
+    </Link>
   );
 };
 
