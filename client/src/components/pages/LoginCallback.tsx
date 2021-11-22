cecd8270e842b8e8d947cc4962ed3b4c1a86f9ff
hayeon <hyeon7340@gmail.com>
2021-11-22 22:06:53 +0900

    Sign Up if user not exist in DB

--- client/src/components/pages/LoginCallback.tsx
+++ client/src/components/pages/LoginCallback.tsx
@@ -1,7 +1,7 @@
 // import React, { useEffect, useState } from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Redirect, Route, Link } from 'react-router-dom';
-import { signUp } from '../../lib/api/member';
+import { loginOrSignup, getAllDibs } from '../../lib/api/member'
 
 interface Window {
   [key: string]: any; // Add index signature
@@ -12,7 +12,6 @@ function NaverIdLoginCallback() {
   useEffect(naverLoginCallback, []);
 
   function naverLoginCallback() {
-    console.log("naverLoginCallback()");
     const naverLogin = new naver.LoginWithNaverId({
       //SETTING FOR REQUEST LOGIN
       clientId: 'ngA3r6hcze4XQpin7Qrr',
@@ -28,43 +27,30 @@ function NaverIdLoginCallback() {
 
     // GET ID/NAME/EMAIL IF LOGIN SUCCESS
     naverLogin.getLoginStatus((status: any) => {
-      console.log("status = ", status);
       if (status) {
         const { id, name, email } = naverLogin.user; // PROFILE
         if (name === undefined || email === undefined) {
           alert('이름, 이메일 정보는 필수 동의입니다. 정보제공을 동의해주세요');
           naverLogin.reprompt();
         } else {
-          // SOCIAL LOGIN SUCCESS
+          // LOGIN SUCCESS
           sessionStorage.setItem('id', id);
           sessionStorage.setItem('name', name);
           sessionStorage.setItem('email', email);
 
-          console.log("SET SESSION");
-          signUp(id);
-          // const ret = async () => { console.log("LOGIN SUCCESS at LoginCallBack"); signUp(id); }
+          const login = (async () => {
+            loginOrSignup(id);
+          });
+          login();
         }
       }
     });
   }
 
-  if (sessionStorage.getItem('name') !== undefined) {
-    // REDIRECT TO HOMEPAGE
-    console.log("LOGIN SUCCESS at LoginCallBack - redirect");
-    return (
-      <div>
-        {/* <Redirect to={{ pathname: `/home` }} /> */}
-        <div id="naverIdLogin" onClick={NaverIdLoginCallback}>
-          네이버 로그인
-        </div>
-      </div>
-    );
-  }
-
   return (
     // REDIRECT TO LOGINPAGE
     <div>
-      <Redirect to={{ pathname: `/` }} />
+      <Redirect to={{ pathname: `/home` }} />
       <div id="naverIdLogin" onClick={NaverIdLoginCallback}>
         네이버 로그인
       </div>
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -4,7 +4,7 @@ import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
-import { Logger } from '@nestjs/common';
+import { Logger, ConflictException } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
@@ -22,38 +22,14 @@ export class UsersService {
   }
 
   async create(createUserDto: CreateUserDto) {
-    Logger.log("CREATE - service.ts");
+    if ((await this.UserModel.findOne({ _id: createUserDto._id }))) {
+      throw new ConflictException();
+    }
     const newUser = await new this.UserModel(createUserDto);
-    Logger.log("CREATE new usermodel");
-    return newUser.save()
+    return newUser.save();
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
-    const mode = updateUserDto.data.mode;
-    if (mode === 1) { // ADD DIBS
-      this.UserModel.findByIdAndUpdate(id, {
-        $push: { "productId": updateUserDto.data.productId },
-        function(error, success) {
-          if (error) {
-            console.log("ERROR AT ADD DIBS TO USER," + error)
-          } else {
-            console.log("SUCCESS TO ADD DIBS TO USER");
-          }
-        }
-      })
-    } else if (mode === 2) { // DELETE DIBS
-      this.UserModel.findByIdAndUpdate(id, {
-        $pull: { "productId": updateUserDto.data.productId },
-        function(error, success) {
-          if (error) {
-            console.log("ERROR AT REMOVE DIBS TO USER," + error)
-          } else {
-            console.log("SUCCESS TO REMOVE DIBS TO USER");
-          }
-        }
-      })
-    }
-
     return 'test-user';
   }
 }
