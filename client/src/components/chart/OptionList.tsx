25bfd5b848249fc257366819a3dbb1f329887f1e
Soulmates2 <kaist984@kaist.ac.kr>
2021-11-23 02:07:02 +0900

    차트 샘플

--- client/src/components/chart/OptionList.tsx
+++ client/src/components/chart/OptionList.tsx
@@ -1,29 +1,45 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 // import { getProducts } from '../../lib/api/shopping';
 // import { useRecoilState } from 'recoil';
 // import { productState } from './states/product.state';
 // import Product from '../shopping/Product';
+import { Radio } from 'antd';
+
+// function onChange(e: any) {
+//   console.log(`checked = ${e.target.checked}`);
+// }
+
 
 interface OptionProps {
-    optionID : string;
-  }
-  const OptionList = (props: OptionProps) => {
-    // const [OptionList, setOptionList] = useState([]);
-    // const [productName] = useRecoilState(productState);
-  
-    const options = props.optionID
-  
-    //옵션 리스트를 만듭니다.
-    return (
-      <div className="optionList">
-        <h1>분야 통계</h1>
-        <h2>기간</h2>
-        <option>일간</option>
-        <option>주간</option>
-        <option>월간</option>
-      </div>
-    );
+  optionID : string;
+}
+
+
+const OptionList = (props: OptionProps) => {
+  // const [OptionList, setOptionList] = useState([]);
+  // const [productName] = useRecoilState(productState);
+
+  const options = props.optionID
+  const [option, setOption] = useState(1);
+
+
+  const handleInputChange = (value: any) => {
+    setOption(value);
   };
-  
-  export default OptionList;
+
+  //옵션 리스트를 만듭니다.
+  return (
+    <div className="optionList">
+      <h1>분야 통계</h1>
+      <h2>기간</h2>
+      <Radio.Group>
+        <Radio value={1} onChange={e => handleInputChange(1)}>일간</Radio>
+        <Radio value={2} onChange={e => handleInputChange(2)}>주간</Radio>
+        <Radio value={3} onChange={e => handleInputChange(3)}>월간</Radio>
+      </Radio.Group>
+    </div>
+  );
+};
+
+export default OptionList;
   
