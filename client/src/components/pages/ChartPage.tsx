bb1568863dd5ae38f69959b899d203c3d98aafee
xymon2 <vbbn2030@kaist.ac.kr>
2021-12-05 05:11:01 +0900

    conflict 수정

--- client/src/components/pages/ChartPage.tsx
+++ client/src/components/pages/ChartPage.tsx
@@ -3,6 +3,7 @@ import { RouteComponentProps } from 'react-router-dom';
 import OptionList from '../chart/OptionList';
 import WrongAccess from './WrongAccess';
 import bb, { area, bar, line, zoom } from 'billboard.js';
+import 'billboard.js/dist/billboard.css';
 import 'antd/dist/antd.css';
 
 interface ProductInfoProps {
@@ -17,46 +18,77 @@ interface ProductInfoProps {
   view: object;
 }
 
-const ChartPage = (props: RouteComponentProps<{}, {}, ProductInfoProps>) => {
-  const { state } = props.location;
-  var chart = bb.generate({
-    bindto: '#linechart',
-    data: {
-      // for ESM import usage, import 'line' module and execute it as
-      type: line(),
-      // type: "line",
-      json: [
-        { date: '11/10/2021, 20:25', 조회수: 12, '찜 횟수': 2 },
-        { date: '11/11/2021, 20:23', 조회수: 22, '찜 횟수': 2 },
-        { date: '11/11/2021, 18:10', 조회수: 7, '찜 횟수': 2 },
-        { date: '11/12/2021, 17:11', 조회수: 8, '찜 횟수': 2 },
-        { date: '11/22/2021, 12:00', 조회수: 4, '찜 횟수': 2 },
+var chart = bb.generate({
+  data: {
+    type: line(),
+    x: 'x',
+    xFormat: '%Y-%m-%d %H:%M',
+    columns: [
+      // ["x", "2021-11-10", "2021-11-12", "2021-11-13", "2021-11-14", "2021-11-22"],
+      [
+        'x',
+        '2021-11-10 20:25',
+        '2021-11-11 18:10',
+        '2021-11-11 20:23',
+        '2021-11-12 17:11',
+        '2021-11-22 12:00',
       ],
-      keys: {
-        // x: "date", // it's possible to specify 'x' when category axis
-        value: ['조회수', '찜 횟수'],
+      // ["x", "2021-11-10 20:25:00", "2021-11-11 18:10:00", "2021-11-11 20:23:00", "2021-11-12 17:11:00", "2021-11-22 12:00:00"],
+      ['조회수', 12, 22, 7, 8, 4],
+      ['찜 횟수', 2, 2, 2, 2, 1],
+    ],
+  },
+  axis: {
+    x: {
+      type: 'timeseries',
+      tick: {
+        // "format": "%Y-%m-%d",
+        format: '%Y-%m-%d %H:%M',
+        // "format": "%Y-%m-%d %H:%M:%S",
+        // "format": function(x: any) {
+        //   return x.getMonth();
+        // },
+        multiline: true,
       },
     },
-    padding: {
-      top: 20,
-      bottom: 20,
-      left: 100,
-      right: 100,
-    },
+  },
+  bindto: '#chart',
+  padding: {
+    top: 20,
+    bottom: 20,
+    left: 100,
+    right: 100,
+  },
+  zoom: {
+    // for ESM import usage, import 'zoom' module and execute it as
+    enabled: zoom(),
+    // enabled: true
+  },
 
-    zoom: {
-      // for ESM import usage, import 'zoom' module and execute it as
-      enabled: zoom(),
-      // enabled: true
-    },
-  });
+  // "color": {
+  //   "pattern": [
+  //     "red",
+  //     "blue",
+  //     "cyan"
+  //   ]
+  // }
+});
+
+const ChartPage = (props: RouteComponentProps<{}, {}, ProductInfoProps>) => {
+  const { state } = props.location;
   return (
-    <div className="chartPage">
-      {state ? <h1>{state.name} 상품의 trend chart</h1> : <WrongAccess />}
-      <OptionList optionID={'1'}></OptionList>
-      <div id="chart">
-        <script type="text/javascript">chart.load(...)</script>
-      </div>
+    <div className="ChartPage">
+      {state ? (
+        <div>
+          <h1>{state.name} 상품의 trend chart</h1>
+          <OptionList optionID={'1'}></OptionList>
+          <div id="chart">
+            <script type="text/javascript">chart.load(...)</script>
+          </div>
+        </div>
+      ) : (
+        <WrongAccess />
+      )}
     </div>
   );
 };
